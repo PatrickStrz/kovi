e8697c9432fe924f790024af21aac0731aa9863d
Patrick Strzelec <PatrickStrz@users.noreply.github.com>
2017-05-17 23:38:10 -0400

    router working (#16)

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,15 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <Site>
         <BrowserRouter>
-            {/* <div className="grid-center"> */}
+          <Site>
             <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" render={() => <h1>about</h1>} />
-                {/* <Route path="/post/:id" component={PostDetail} /> */}
-                <Route render={() => <h1>Page Not Found</h1>}/>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" render={() => <h1>about</h1>} />
+              <Route render={() => <h1>Page doesn't exist  ¯\_(ツ)_/¯</h1>}/>
             </Switch>
-            {/* </div> */}
+          </Site>
         </BrowserRouter>
-      </Site>
       </MuiThemeProvider>
     )
   }
