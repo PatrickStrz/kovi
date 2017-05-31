c56e0a4abb9d5792072b9f4397f24db9ed649cf2
PatrickStrz <patrick.strzelec@gmail.com>
2017-05-31 17:30:34 -0400

    deleted unnecessary cofe

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,7 @@ import {muiTheme} from '../lib/theme/mui-theme'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Site from './Site'
 import Home from './Home'
-import GridTest from './GridTest'
 import Layout from './dev/Layout'
-// import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class App extends Component {
   devRoute = `/${process.env.REACT_APP_DEV_ROUTE}/`
@@ -15,18 +13,15 @@ class App extends Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <BrowserRouter>
-          {/* <Grid fluid> */}
           <Site>
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route exact path="/grid" component={GridTest} />
               <Route exact path="/about" render={() => <h1>about</h1>} />
               <Route exact path={this.devRoute} render={() => <h1>Secret dev page</h1>}/>
               <Route exact path={this.devRoute + 'layout'} component={Layout}/>
               <Route render={() => <h1>Page doesn't exist  ¯\_(ツ)_/¯</h1>}/>
             </Switch>
           </Site>
-        {/* </Grid> */}
         </BrowserRouter>
       </MuiThemeProvider>
     )
