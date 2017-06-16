9800e57392c342461a134fc6024e8df571d697d1
PatrickStrz <patrick.strzelec@gmail.com>
2017-06-16 02:22:34 -0400

    fixed up dev layout

--- src/components/dev/Layout.js
+++ src/components/dev/Layout.js
@@ -14,7 +14,7 @@ const styles = {
   },
   col2: {
     backgroundColor:'#40cbff',
-    height:300,
+    height:150,
     borderRadius:5,
   },
   row: {
@@ -35,7 +35,7 @@ const Layout = () => {
               <Row>
                 {cards.map((card)=>{
                 return(
-                  <Col style={styles.col} key={card} xs={12} lg={6}>
+                  <Col style={styles.col} key={card} xs={12} lg={12}>
                     <div> {/* ChallengeCard.js */}
                       <div style={styles.col2}>card#:{card}</div>
                     </div>
@@ -46,7 +46,7 @@ const Layout = () => {
               <Row>
                 <Col xsOffset={5} xs={1}>
                   <button>
-                    NEW problem
+                    New challenge
                   </button>
                 </Col>
               </Row>
