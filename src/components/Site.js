f5cc4bc4863e4deca9b515df2b2a2ddb832f3fbc
Patrick Strzelec <PatrickStrz@users.noreply.github.com>
2017-06-18 23:44:05 -0400

    init (#38)

--- src/components/Site.js
+++ src/components/Site.js
@@ -8,7 +8,7 @@ import {login} from '../lib/auth'
 import SyncUser from './SyncUser'
 import {Grid} from 'react-flexbox-grid'
 import BottomBar from './BottomBar'
-import '../stylesheets/layout.css'
+// import '../stylesheets/layout.css'
 
 class Site extends Component {
 
