a18c0d06d42c981cbbc2f485ef1dc2da5fa416cb
PatrickStrz <patrick.strzelec@gmail.com>
2017-06-21 13:04:23 -0400

    changed bottom bar icons and fixed icon focus

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -5,13 +5,14 @@ import { showCreateChallengeView } from '../actions/challenge-actions'
 
 import {requireAuth} from '../lib/auth'
 
-import FontIcon from 'material-ui/FontIcon'
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation'
 import Paper from 'material-ui/Paper'
-import IconLocationOn from 'material-ui/svg-icons/communication/location-on'
-const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>
-const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>
-const nearbyIcon = <IconLocationOn />
+import AddCircle from 'material-ui/svg-icons/content/add-circle'
+import Notifications from 'material-ui/svg-icons/social/notifications'
+import Public from 'material-ui/svg-icons/social/public'
+const NotificationsIcon = <Notifications />
+const AddIcon = <AddCircle />
+const PublicIcon = <Public />
 
 class BottomBar extends Component {
   state = {
@@ -26,23 +27,28 @@ class BottomBar extends Component {
 
   select = (index) => this.setState({selectedIndex: index})
 
+  handleNewChallenge = (index) => {
+    this.props.showCreateChallengeView()
+    this.setState({selectedIndex: index})
+  }
+
   render() {
     return (
       <Paper zDepth={1} style={this.styles.body}>
         <BottomNavigation selectedIndex={this.state.selectedIndex} >
           <BottomNavigationItem
-            label="Create Challenge"
-            icon={recentsIcon}
-            onTouchTap={()=>requireAuth(this.props.showCreateChallengeView)}
+            label="New Challenge"
+            icon={AddIcon}
+            onTouchTap={()=>requireAuth(this.handleNewChallenge(0))}
           />
           <BottomNavigationItem
-            label="Favorites"
-            icon={favoritesIcon}
+            label="Notifications"
+            icon={NotificationsIcon}
             onTouchTap={() => this.select(1)}
           />
           <BottomNavigationItem
-            label="Nearby"
-            icon={nearbyIcon}
+            label="Community"
+            icon={PublicIcon}
             onTouchTap={() => this.select(2)}
           />
         </BottomNavigation>
