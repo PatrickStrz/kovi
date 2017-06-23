a9626db5d8c051909299032dfe6f8d178e388a02
PatrickStrz <patrick.strzelec@gmail.com>
2017-06-23 17:59:33 -0400

    action button trigger new challenge form

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import { bindActionCreators } from 'redux'
+// import {connect} from 'react-redux'
+// import { bindActionCreators } from 'redux'
 import { showCreateChallengeView } from '../actions/challenge-actions'
 
 import {requireAuth} from '../lib/auth'
 
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation'
 import Paper from 'material-ui/Paper'
-// import AddCircle from 'material-ui/svg-icons/content/add-circle'
 import FilterList from 'material-ui/svg-icons/content/filter-list'
 import Notifications from 'material-ui/svg-icons/social/notifications'
 import Public from 'material-ui/svg-icons/social/public'
@@ -28,20 +27,14 @@ class BottomBar extends Component {
 
   select = (index) => this.setState({selectedIndex: index})
 
-  handleNewChallenge = () => {
-    this.props.showCreateChallengeView()
-    this.setState({selectedIndex: 0})
-  }
-
   render() {
-    const handleNewChallengeCb = () => this.handleNewChallenge(0)
     return (
       <Paper zDepth={1} style={this.styles.body}>
         <BottomNavigation selectedIndex={this.state.selectedIndex} >
           <BottomNavigationItem
             label="Filter"
             icon={FilterIcon}
-            onTouchTap={()=>requireAuth(handleNewChallengeCb)}
+            onTouchTap={() => this.select(0)}
           />
           <BottomNavigationItem
             label="Notifications"
@@ -59,10 +52,10 @@ class BottomBar extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({
-      showCreateChallengeView
-    }, dispatch)
-}
+// const mapDispatchToProps = (dispatch) => {
+//     return bindActionCreators({
+//       showCreateChallengeView
+//     }, dispatch)
+// }
 
-export default connect(null, mapDispatchToProps)(BottomBar)
+export default BottomBar
