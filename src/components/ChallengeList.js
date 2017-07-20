ced5fbaf844068cfb69d87843cd5d426bd28cd2f
Patrick Strzelec <PatrickStrz@users.noreply.github.com>
2017-07-20 14:23:05 -0400

    Revert "testing modal (#91)"
    This reverts commit e16370bcecfdd23d697c7bcdb0c7dc174268ed9f.

--- src/components/ChallengeList.js
+++ src/components/ChallengeList.js
@@ -22,7 +22,6 @@ import ChallengeCreateForm from './ChallengeCreateForm'
 import {Row, Col} from 'react-flexbox-grid'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import Modal from './Modal'
-import Modal2 from './Modal2'
 import GenericError from './commons/GenericError'
 import Editor from './Editor'
 
@@ -65,6 +64,7 @@ class ChallengeList extends Component {
       })
     }
   }
+
   render(){
     if (this.props.loading){
       return(<div>
@@ -111,20 +111,14 @@ class ChallengeList extends Component {
             mdOffset={3} md={6}
             lgOffset={3} lg={6}
           >
-            {/* <Modal
+            <Modal
               isOpen={this.props.isCreateViewOpen}
               handleClose={this.props.hideCreateChallengeView}
               title='Create A Challenge'
               repositionOnUpdate={false}
             >
               {createChallengeForm}
-            </Modal> */}
-            {this.props.isCreateViewOpen && <Modal2
-              handleClose={this.props.hideCreateChallengeView}
-             >
-               {createChallengeForm}
-             </Modal2> }
-
+            </Modal>
             <Row>
             {challengeCards}
             </Row>
