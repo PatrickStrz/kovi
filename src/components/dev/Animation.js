bd557c796f057f4a93da03ca389b45443fc53df2
PatrickStrz <patrick.strzelec@gmail.com>
2017-07-28 17:08:04 -0400

    minor fix

--- src/components/dev/Animation.js
+++ src/components/dev/Animation.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import MoUpvote from '../MoUpvote'
-import AnimationContainer from '../AnimationContainer'
 
 const Animation = () => <div><MoUpvote/></div>
 export default Animation
