419a6cb92c812e43a46b3efbeea5df6939d67c8e
PatrickStrz <patrick.strzelec@gmail.com>
2017-08-08 20:07:17 -0400

    comments styled

--- src/components/ChallengeCommentsContainer.js
+++ src/components/ChallengeCommentsContainer.js
@@ -5,16 +5,47 @@ import PropTypes from 'prop-types'
 import {graphql} from 'react-apollo'
 import {COMMENTS_ON_CHALLENGE} from '../gql/Comment/queries'
 //helpers+other
-// import {muiColors} from '../lib/theme/colors'
 import {logException} from '../config'
 import styled from 'styled-components'
 import {muiColors} from 'lib/theme/colors'
 //components
 import GenericError from './commons/GenericError'
 
-const ShowChildrenButton = styled.a`
-  color: ${muiColors.primary1};
+const ShowChildrenButton = styled.p`
+  color: ${muiColors.secondary1};
   cursor: pointer;
+`
+
+const UserName = styled.div`
+  color: ${muiColors.primary1};
+  display: inline-block;
+  position: relative;
+  bottom: 6px;
+  left: 5px;
+`
+
+
+const CommentText = styled.p`
+  color: rgb(52, 52, 52);
+  display: inline;
+  position: relative;
+  left: 15px;
+  top: 50%;
+  word-wrap: break-word;
+`
+const UserBox = styled.div`
+  position: relative;
+`
+
+const Avatar = styled.div`
+  background-image: url('http://i.stack.imgur.com/Dj7eP.jpg');
+  width: 30px;
+  height: 30px;
+  background-size: cover;
+  /* center the image vertically and horizontally */
+  background-position: center;
+  border-radius: 50%;
+  display: inline-block;
 `
 
 class ChallengeCommentsContainer extends Component{
@@ -31,7 +62,13 @@ class ChallengeCommentsContainer extends Component{
     return (comments.map(comment =>{
       return(
         <div key={'comment'+comment.id}>
-          <div>{comment.text}</div>
+        <UserBox>
+          <Avatar/>
+          <UserName>
+            {comment.user.name}
+          </UserName>
+          <CommentText>dfkmlsdfaklnkldsafnklndasklfnklsdnafklnsdklfnklsdaflknsdaklfndsklfnldksnflkdsnflkdsnfklndslkfndkfnkdfkdkfkdfkdfkndkfdkfnkdfnkdnfkdnfkdnfkdnfkndkls</CommentText>
+        </UserBox>
           <ShowChildrenButton
             onClick={()=>{this.setState({showChildComments:true})}}
           >
@@ -46,7 +83,9 @@ class ChallengeCommentsContainer extends Component{
   renderChildComments = (comments) => {
     return(comments.map( comment =>{
       return(
-        <div>{comment.text}</div>
+      <div key={'subcomment' + comment.id}>
+        <CommentText>{comment.text}</CommentText>
+      </div>
         )
       })
     )
