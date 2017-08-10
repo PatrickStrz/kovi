b242a110ba3c252ca6ee58f7d04806b395172804
PatrickStrz <patrick.strzelec@gmail.com>
2017-08-09 20:00:53 -0400

    fixed alignment of comment section

--- src/components/ChallengeCommentsContainer.js
+++ src/components/ChallengeCommentsContainer.js
@@ -10,44 +10,23 @@ import styled from 'styled-components'
 import {muiColors} from 'lib/theme/colors'
 //components
 import GenericError from './commons/GenericError'
-import Avatar from 'ui-components/Avatar'
+import UserHeader from 'ui-components/UserHeader'
 
 const ShowChildrenButton = styled.p`
   color: ${muiColors.secondary1};
   cursor: pointer;
 `
 
-const UserName = styled.div`
-  color: ${muiColors.primary1};
-  display: inline-block;
-  position: relative;
-  bottom: 6px;
-  left: 5px;
-`
-
-
 const CommentText = styled.p`
-  color: rgb(52, 52, 52);
-  display: inline;
-  position: relative;
-  left: 15px;
-  top: 50%;
+  color: #545252;
   word-wrap: break-word;
 `
-const UserBox = styled.div`
-  position: relative;
-`
 
-// const Avatar = styled.div`
-//   background-image: url('http://i.stack.imgur.com/Dj7eP.jpg');
-//   width: 30px;
-//   height: 30px;
-//   background-size: cover;
-//   /* center the image vertically and horizontally */
-//   background-position: center;
-//   border-radius: 50%;
-//   display: inline-block;
-// `
+const CommentSectionContainer = styled.div`
+  margin: auto;
+  justify-content: center;
+  width: 80%;
+`
 
 class ChallengeCommentsContainer extends Component{
   static propTypes = {
@@ -62,21 +41,20 @@ class ChallengeCommentsContainer extends Component{
     const comments = this.props.data.allComments
     return (comments.map(comment =>{
       return(
-        <div key={'comment'+comment.id}>
-        <UserBox>
-          <Avatar imageUrl={comment.user.picture}/>
-          <UserName>
-            {comment.user.name}
-          </UserName>
-          <CommentText>dfkmlsdfaklnkldsafnklndasklfnklsdnafklnsdklfnklsdaflknsdaklfndsklfnldksnflkdsnflkdsnfklndslkfndkfnkdfkdkfkdfkdfkndkfdkfnkdfnkdnfkdnfkdnfkdnfkndkls</CommentText>
-        </UserBox>
-          <ShowChildrenButton
-            onClick={()=>{this.setState({showChildComments:true})}}
-          >
-            Show Children
-          </ShowChildrenButton>
-          {this.state.showChildComments && this.renderChildComments(comment.childComments)}
-        </div>
+          <CommentSectionContainer key={'comment'+comment.id}>
+            <UserHeader
+              imageUrl={comment.user.picture}
+              userName={comment.user.name}
+            />
+            <CommentText>dfkmlsdfaklnkldsafnklndasklfnklsdnafklnsdklfnklsdaflknsdaklfndsklfnldksnflkdsnflkdsnfklndslkfndkfnkdfkdkfkdfkdfkndkfdkfnkdfnkdnfkdnfkdnfkdnfkndkls</CommentText>
+            <ShowChildrenButton
+              onClick={()=>{this.setState({showChildComments:true})}}
+            >
+              Show Children
+            </ShowChildrenButton>
+            {this.state.showChildComments && this.renderChildComments(comment.childComments)}
+
+          </CommentSectionContainer>
       )
     }))
   }
--- src/ui-components/UserHeader.js
+++ src/ui-components/UserHeader.js
@@ -1,17 +1,48 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
+//components
 import Avatar from 'ui-components/Avatar'
+import {muiColors, colors} from 'lib/theme/colors'
 
-const Container = styled.div`
+
+
+const OuterContainer = styled.div`
   display: flex;
   align-items: center;
 `
-const UserHeader = () => {
+
+const UserName = styled.div`
+  color: ${muiColors.primary1};
+`
+
+const Date = styled.div`
+  color: ${colors.lightGrey};
+  font-size: 14px;
+`
+
+const TextContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  margin-left: 5px;
+`
+// Component that consists of user avatar with user name beside
+const UserHeader = (props) => {
  return(
-   <Container>
-     <Avatar></Avatar>
-   </Container> 
+   <OuterContainer>
+     <Avatar imageUrl={props.imageUrl} />
+     <TextContainer>
+       <UserName>{props.userName}</UserName>
+       <Date>Jun 1</Date>
+     </TextContainer>
+   </OuterContainer>
  )
+}
+
+UserHeader.propTypes = {
+  userName: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
 }
 
 export default UserHeader
