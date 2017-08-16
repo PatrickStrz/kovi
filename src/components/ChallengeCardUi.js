359d9cc29822a27f42badd051d512a22e68378ca
PatrickStrz <patrick.strzelec@gmail.com>
2017-08-16 14:34:07 -0400

    deleted old colde

--- src/components/ChallengeCardUi.js
+++ src/components/ChallengeCardUi.js
@@ -4,12 +4,10 @@ import styled from 'styled-components'
 
 const CardBox = styled.div`
   background-color: white;
-  width:100%;
-  padding: 10px;
   :hover{
     background-color: #f6f0f0;
   }
-  cursor: pointer;
+  width:100%;
   transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
   box-sizing: border-box;
   font-family: Roboto, sans-serif;
@@ -19,31 +17,31 @@ const CardBox = styled.div`
   z-index: 1;
   margin-bottom: 5px;"
 `
+const CardBody = styled.div`
+  padding: 10px;
+  cursor: pointer;
+`
 const Text = styled.p`
   font-size: 16px;
   color: rgb(75, 75, 75)
 `
-const ActionsBox = styled.div``
+const ActionsBox = styled.div`
+`
 
 export default class ChallengeCardUi extends Component{
 
   static propTypes = {
     text: PropTypes.string.isRequired,
     onUpvoteClick: PropTypes.func,
-    // actions: PropTypes.array.isRequired,
   }
 
   render(){
-
-    const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.`
-
     const {text, upvote, onClick} = this.props
     return(
-      <CardBox onClick={()=>onClick()}>
-        <Text>{lorem}</Text>
+      <CardBox>
+        <CardBody onClick={onClick}>
+          <Text>{text}</Text>
+        </CardBody>
         <ActionsBox>
           {upvote}
         </ActionsBox>
