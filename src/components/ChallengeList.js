dd8cc993d718771b78845f8cd4ce34707409ea5d
Patrick Strzelec <PatrickStrz@users.noreply.github.com>
2017-08-17 01:43:51 -0400

    moved infinite scroll to challengeLlist (#135)

--- src/components/ChallengeList.js
+++ src/components/ChallengeList.js
@@ -1,8 +1,10 @@
+// React
 import React,{Component} from 'react'
 import PropTypes from 'prop-types'
 //components
 import ChallengeCard from './ChallengeCard'
-import {Col} from 'react-flexbox-grid'
+import {Col, Row} from 'react-flexbox-grid'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 
 export default class ChallengeList extends Component {
@@ -10,11 +12,19 @@ export default class ChallengeList extends Component {
     challenges: PropTypes.array.isRequired,
     apiUserId: PropTypes.string.isRequired,
     allChallengesQueryVariables: PropTypes.object.isRequired,
+    loadMoreEntries: PropTypes.func.isRequired,
+    hasMore: PropTypes.bool.isRequired,
   }
-  renderChallengeCards = () =>{
-    const {challenges, allChallengesQueryVariables, apiUserId} = this.props
 
-    return challenges.map(challenge =>{
+  renderChallengeCards = () => {
+
+    const {
+      challenges,
+      allChallengesQueryVariables,
+      apiUserId,
+    } = this.props
+
+    return challenges.map(challenge => {
       return(
         <Col key={'challengelist'+challenge.id} xs={12}>
           <ChallengeCard
@@ -23,14 +33,34 @@ export default class ChallengeList extends Component {
             allChallengesQueryVariables={allChallengesQueryVariables}
           />
         </Col>
-      )
-    })
+        )
+      }
+    )
   }
+
+  /* ---------------- Render -----------------*/
+
   render(){
+    const {hasMore, loadMoreEntries} = this.props
+
     return(
-      <div>
-        {this.renderChallengeCards()}
-      </div>
+      <InfiniteScroll
+        pageStart={0}
+        hasMore={hasMore}
+        loader={<div className="loader">Loading ...</div>}
+        next={loadMoreEntries}
+       >
+        <Col
+          xsOffset={1} xs={10}
+          smOffset={1} sm={10}
+          mdOffset={3} md={6}
+          lgOffset={3} lg={6}
+        >
+          <Row>
+            {this.renderChallengeCards()}
+          </Row>
+        </Col>
+      </InfiniteScroll>
     )
   }
 }
