71d7e024f1567f1f029bfaa0219b80bbf8294bd3
PatrickStrz <patrick.strzelec@gmail.com>
2017-08-18 03:58:54 -0400

    Generic error centered

--- src/ui-kit/GenericError.js
+++ src/ui-kit/GenericError.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import {muiColors} from 'styles/theme/colors'
+import styled from 'styled-components'
 
-const GenericError = () =>
-<div>
-  <h1 style={{color:muiColors.primary1}}>
-    Request Error (¬▂¬)
-  </h1>
-  <h4>Please check your connection</h4>
-</div>
+const Box = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-basis: 100%;
+  align-items: center;
+  flex-direction: column;
+`
 
+const GenericError = () =>{
+  return(
+    <Box>
+      <h1 style={{color:muiColors.primary1}}>
+        Request Error (¬▂¬)
+      </h1>
+      <h4>Please check your connection</h4>
+    </Box>
+  )
+}
 
 export default GenericError
