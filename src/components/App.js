2c7b136b66663e6d5e4fe8902789d975b2a4d73c
PatrickStrz <patrick.strzelec@gmail.com>
2017-08-21 12:53:15 -0400

    removed react-flexbox-grid dependency + removed dev layouts

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,7 @@ import {muiTheme} from 'styles/theme/mui-theme'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Site from './Site'
 import Home from './Home'
-import Layout from './dev/Layout'
-import HomeLayout from './dev/HomeLayout'
+// import HomeLayout from './dev/HomeLayout'
 
 class App extends Component {
   devRoute = `/${process.env.REACT_APP_DEV_ROUTE}/`
@@ -15,16 +14,12 @@ class App extends Component {
       <MuiThemeProvider muiTheme={muiTheme}>
         <BrowserRouter>
           <Site>
-            {/* <div> */}
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" render={() => <h1>about</h1>} />
                 <Route exact path={this.devRoute} render={() => <h1>Secret dev page</h1>}/>
-                <Route exact path={this.devRoute + 'layout'} component={Layout}/>
-                <Route exact path={this.devRoute + 'layout/home'} component={HomeLayout}/>
                 <Route render={() => <h1>Page doesn't exist  ¯\_(ツ)_/¯</h1>}/>
               </Switch>
-            {/* </div> */}
           </Site>
         </BrowserRouter>
       </MuiThemeProvider>
