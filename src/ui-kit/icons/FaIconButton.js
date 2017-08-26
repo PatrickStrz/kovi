58e7063a650400d1a0576ba1359c60a56e3e675b
PatrickStrz <patrick.strzelec@gmail.com>
2017-08-26 12:12:39 -0400

    removed div background highlight

--- src/ui-kit/icons/FaIconButton.js
+++ src/ui-kit/icons/FaIconButton.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import styled, {css} from 'styled-components'
 
@@ -13,6 +13,8 @@ import styled, {css} from 'styled-components'
 }
 
 const Container = styled.div`
+  /* remove background highlight on mobile click: */
+  -webkit-tap-highlight-color:transparent;
   cursor: pointer;
    ${ props => css`
       .fa{
