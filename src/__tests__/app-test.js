d27b5eedc6da3c28708c60efb306ee15aab35839
PatrickStrz <patrick.strzelec@gmail.com>
2017-09-06 21:14:57 -0400

    added enzyme for testing

---
+++ src/__tests__/app-test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import App from 'components/App'
+import { shallow, mount, render } from 'enzyme'
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+    //expect(shallow(<Foo />).contains(<div className="foo" />)).toBe(true);
+  });
+
+  it("contains spec with an expectation", function() {
+    //expect(shallow(<Foo />).is('.foo')).toBe(true);
+  });
+
+  it("contains spec with an expectation", function() {
+    //expect(mount(<Foo />).find('.foo').length).toBe(1);
+  });
+});
