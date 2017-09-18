cb2c844bc62cc8ad2bbb2f6cc023cd0c97c918a2
PatrickStrz <patrick.strzelec@gmail.com>
2017-09-18 13:56:45 -0400

    v1 complete

--- src/ui-kit/ProgressMeter.js
+++ src/ui-kit/ProgressMeter.js
@@ -1,17 +1,15 @@
 import React, {Component} from 'react'
 // import PropTypes from 'prop-types'
 //other
-import styled, {css} from 'styled-components'
-import {muiColors, colors} from 'styles/theme/colors'
+import styled from 'styled-components'
+import {colors, muiColors} from 'styles/theme/colors'
 //other
-import {media} from 'styles/media-queries'
-import {MORE_INFO_SHADOW} from 'styles/shadows'
 
 const Box = styled.div`
   height: 10px;
   width: 100px;
   padding: 0px;
-  border: solid 3px ${muiColors.primary1};
+  border: solid 3px ${colors.lightGrey};
   border-radius: 3px;
   margin: 5px;
 `
@@ -19,7 +17,7 @@ const Progress = styled.div`
   height: 10px;
   width: ${props => `${props.percent}%`};
   margin: 0px;
-  background-color: #00f7f9;
+  background-color: ${muiColors.tertiary1};
   border-top-left-radius: 3px;
   border-bottom-left-radius: 3px;
 `
@@ -27,7 +25,6 @@ const Progress = styled.div`
 class ProgressMeter extends Component {
   // static PropTypes
   render(){
-    const size = '20px'
     return(
       <Box>
         <Progress percent={80} />
