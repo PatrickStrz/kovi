07efbe882fd756df71cec6ac8e596278d32e0b08
PatrickStrz <patrick.strzelec@gmail.com>
2017-10-13 13:19:16 -0400

    v1

---
+++ src/components/RouteChange.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react'
+import {withRouter} from 'react-router-dom'
+import ReactGA from 'react-ga'
+
+/* This component keeps track of Route Changes because onUpdate
+attribute of BrowserRouter removed in react-router 4 api*/
+
+class RouteChange extends Component {
+  componentDidMount = () => {
+    //on initial render:
+    process.env.REACT_APP_ENV === 'production' && this.logPageView()
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    //on every RouterChange:
+    process.env.REACT_APP_ENV === 'production' && this.logPageView()
+  }
+
+  logPageView = () => {
+    ReactGA.set({ page: window.location.pathname + window.location.search })
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }
+
+  render(){
+    return(
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default withRouter(RouteChange)
